d40d695986e192cfdca3a618f1ed9d2b3e4bf16b
maciej <106823716+maciejbrylewicz@users.noreply.github.com>
2022-09-20 18:52:17 +0100

    1

--- run_maildir2mbox_conversion.sh
+++ run_maildir2mbox_conversion.sh
@@ -5,7 +5,7 @@ MAILDIR=/home/maciej/opt
 LOGDIR=/home/maciej/log
 
 for d in "$MAILDIR"/* ; do
-	python3 "$TOOLS_DIR"/maildir2mbox.py -ar "$d" "$d".mboxtst >> "$LOGDIR"/2_progress.log
-	python3 "$TOOLS_DIR"/find_message_id.py "$d" "$d".mboxtst >> "$LOGDIR"/2_diff.log
+	python3 "$TOOLS_DIR"/maildir2mbox.py -ar "$d" "$d".mboxtst >> "$LOGDIR"/1_progress.log
+	python3 "$TOOLS_DIR"/find_message_id.py "$d" "$d".mboxtst >> "$LOGDIR"/1_diff.log
 done
 
